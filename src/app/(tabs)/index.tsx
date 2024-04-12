92ec420f6ecafcfc5e05209dbba70123225f167e
Kamal Minott <kamalminott@gmail.com>
2024-04-12 12:53:10 +0100

    changed tsconfig added default image

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,12 +1,12 @@
 import { View } from "react-native";
-import ProductListItem from "@/src/components/ProductListItem";
-import products from "@/assets/data/products";
+import ProductListItem from "@components/ProductListItem";
+import products from "@assets/data/products";
 
 export default function MenuScreen() {
   return (
     <View>
       <ProductListItem product={products[5]} />
-      <ProductListItem product={products[1]} />
+      <ProductListItem product={products[0]} />
     </View>
     
   );
