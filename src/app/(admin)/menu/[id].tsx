5ddc71d293cdfbd69cbd81e12dde40a468187716
Kamal Minott <kamalminott@gmail.com>
2024-04-18 20:54:39 +0100

    admin site

---
+++ src/app/(admin)/menu/[id].tsx
@@ -0,0 +1,79 @@
+import { Stack, useLocalSearchParams, useRouter } from 'expo-router';
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native'
+import products from '@assets/data/products';
+import { defaultPizzaImage } from '@/components/ProductListItem';
+import { useState } from 'react';
+import Button from '@components/Button';
+import { useCart } from '@/providers/CartProvider';
+import { PizzaSize } from '@/types';
+
+const sizes: PizzaSize[] = ['S', 'M', 'L', 'XL'];
+
+const ProductDetailsScreen =() => {
+  const { id } = useLocalSearchParams();
+  
+  const {addItem} = useCart();
+
+  const router = useRouter();
+
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>('M')
+
+  const product = products.find((p) => p.id.toString() === id)
+
+  const addToCart = () => {
+    if (!product) {
+      return;
+    }
+    addItem(product, selectedSize);
+    router.push('/cart')
+  }
+
+  if(!product) {
+    return <Text>Product not found</Text>
+  }
+
+  return (
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product?.name}} />
+      <Image 
+      style={styles.image}
+      source={{uri: product.image || defaultPizzaImage}}
+      /> 
+
+      <Text style={styles.title}>{product.name}</Text>  
+      <Text style={styles.price}>£{product.price}</Text>
+
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+    padding: 10,
+
+  },
+
+  image:{
+    width:'100%',
+    aspectRatio: 1
+  },
+
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+
+  price: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  
+
+
+})
+
+export default ProductDetailsScreen;
